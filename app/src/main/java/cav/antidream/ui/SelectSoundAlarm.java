098b1f61f6332721cdda762baf8ecb73e584eb92
Alexander <cavinc20@gmail.com>
2018-05-18 09:51:40 +0700

    Добавлен диалог выбора звука

--- app/src/main/java/cav/antidream/ui/SelectSoundAlarm.java
+++ app/src/main/java/cav/antidream/ui/SelectSoundAlarm.java
@@ -1,10 +1,22 @@
 package cav.antidream.ui;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by cav on 18.05.18.
  */
 
 public class SelectSoundAlarm extends DialogFragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
 }
